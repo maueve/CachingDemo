03a6c35aa83e3f9839af93efb03cd33f5e7233ce
MAURICIO EVERARDO DE LA CRUZ SANCHEZ <mdel@elektra.com.mx>
2022-03-14 19:06:42 -0600

    Initial Commit: Demo of using Ecache library on Spring

---
+++ src/main/java/com/maueve/demo/caching/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.maueve.demo.caching;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CachingDemoApplication.class);
+    }
+
+}
